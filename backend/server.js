fbf31c4350519579488d8074378778e6d9e664ab
Sekhar Thapa <sgrthpa457>
2025-06-22 07:06:53 +0545

    fix prod bug

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,11 @@ const __dirname = path.resolve();
 // Enable CORS for frontend dev server
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://smart-tech-bca5.netlify.app",
+    ],
     credentials: true,
   })
 );
@@ -29,13 +33,13 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
 // Content Security Policy middleware (must be before routes and static files)
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "script-src 'self' https://js.stripe.com https://khalti.com blob: 'unsafe-inline';"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Content-Security-Policy",
+//     "script-src 'self' https://js.stripe.com https://khalti.com blob: 'unsafe-inline';"
+//   );
+//   next();
+// });
 
 // API Routes
 app.use("/api/auth", authRoutes);
@@ -47,13 +51,13 @@ app.use("/api/analytics", analyticsRoutes);
 app.use("/api/khalti", khaltiRoutes);
 
 // Serve frontend static files in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
 
 // Start server and connect DB
 app.listen(PORT, () => {
