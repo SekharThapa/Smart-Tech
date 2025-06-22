2a18d6aa09f3d448abdd69bf6e29e96e9640f544
Sekhar Thapa <sgrthpa457>
2025-06-22 07:09:22 +0545

    changed prod url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
-	withCredentials: true, // send cookies to the server
+  baseURL:
+    import.meta.mode === "development"
+      ? "http://localhost:5000/api"
+      : "https://smart-tech-frqx.onrender.com/api",
+  withCredentials: true, // send cookies to the server
 });
 
 export default axiosInstance;
